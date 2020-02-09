31fa3b8d06ec6e9456b31aa2bc6ca55eb57cae35
Andrey <cheyzercheyzer@yandex.ru>
2020-02-09 20:59:45 +0300

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { MainComponent } from './layouts/main/main.component';
+import { SidenavComponent } from './layouts/main/sidenav/sidenav.component';
+import { FeedComponent } from './layouts/main/feed/feed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchFormComponent,
+    ProfileComponent,
+    MainComponent,
+    SidenavComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
