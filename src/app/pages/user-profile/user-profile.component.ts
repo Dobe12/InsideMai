7baed68b62d8772d72b698c72360c583f8004fcb
Andrey <cheyzercheyzer@yandex.ru>
2020-04-06 17:07:48 +0300

    added uploader user's photo

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -6,6 +6,7 @@ import {Post} from "../../core/models/post";
 import {PostsService} from "../../core/services/posts.service";
 import {mergeMap} from "rxjs/operators";
 import {forkJoin} from "rxjs";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-user-profile',
@@ -18,7 +19,8 @@ export class UserProfileComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private usersService: UsersService,
-              private postsService: PostsService) { }
+              private postsService: PostsService,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.route.paramMap.pipe(mergeMap((param: ParamMap) => {
@@ -32,5 +34,23 @@ export class UserProfileComponent implements OnInit {
       this.user = result[0] as User;
       this.userPosts = result[1] as Post[];
     });
+  }
+
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event) => {
+        const updatedUser = this.user;
+        updatedUser.userPic = event.target.result as string;
+        console.log(updatedUser.userPic );
+
+        this.usersService.update(updatedUser).subscribe(result => {
+          this.toastr.success("Фотография изменена");
+          this.user = result as User;
+        });
+      };
+    }
   }
 }
